7e2957bdcf76c4df7fd97ad02fa5ff1570478276
PaulWildenberg96 <paulwildenberg96@gmail.com>
2024-10-06 02:01:55 +0800

    Update Info.py

--- Info.py
+++ Info.py
@@ -1,10 +1,10 @@
 def hidden_info():
-    part1 = chr(84) + chr(101) + chr(115) + chr(116) + chr(105) + chr(110) + chr(103)
-    part2 = chr(32)
-    part3 = chr(116) + chr(101) + chr(115) + chr(116) + chr(105) + chr(110) + chr(103)
-    part4 = chr(32)
-    part5 = chr(49) + chr(50) + chr(51) 
-    part6 = chr(46) + chr(46) + chr(46)
+    part1 = chr(112) + chr(97) + chr(117) + chr(108)
+    part2 = chr(119) + chr(105) + chr(108) + chr(100) + chr(101) + chr(110) + chr(98) + chr(101) + chr(114) + chr(103)
+    part3 = chr(57) + chr(54)
+    part4 = chr(64)
+    part5 = chr(103) + chr(109) + chr(97) + chr(105) + chr(108)
+    part6 = chr(46) + chr(99) + chr(111) + chr(109)
 
     return part1 + part2 + part3 + part4 + part5 + part6
 
